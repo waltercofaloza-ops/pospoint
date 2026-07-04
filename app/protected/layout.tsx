08206e4f6023360ead87eab4728f790ce0c79161
waltercofaloza-ops <walter.cofaloza@gmail.com>
2026-07-04 11:44:08 -0300

    Update layout.tsx

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -1,10 +1,4 @@
-import { EnvVarWarning } from "@/components/env-var-warning";
-import { AuthButton } from "@/components/auth-button";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { hasEnvVars } from "@/lib/utils";
-import { Suspense } from "react";
-// NUEVOS IMPORTS
-import { HabitatProvider } from "@/domain/habitatcontext"; 
+import { HabitatProvider } from "@/domain/habitatcontext";
 import { Header } from "@/components/layout/header";
 import { Footer } from "@/components/layout/footer";
 
@@ -12,13 +6,11 @@ export default function ProtectedLayout({ children }: { children: React.ReactNod
   return (
     <HabitatProvider>
       <main className="min-h-screen flex flex-col">
-        <Header /> {/* Reemplaza el nav original */}
-        
+        <Header /> 
         <div className="flex-1 flex flex-col w-full">
           {children}
         </div>
-
-        <Footer /> {/* Reemplaza el footer original */}
+        <Footer />
       </main>
     </HabitatProvider>
   );
